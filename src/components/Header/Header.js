0ce24990b171d2d01a6d8675b61a27282dcdb6b4
farakib <farakib534@gmail.com>
2021-09-26 00:05:12 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header-container">
+            <h1>National University Bangladesh</h1>
+            <p>Accounting Department Book list..</p>
+            <p>Budget: $6000</p>
+        </div>
+    );
+};
+
+export default Header;
